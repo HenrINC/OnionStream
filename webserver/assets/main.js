c1a1b538ec63ff41ac503fd07aef9016b43ced5e
dev <dev@dev-server.voo.be>
2023-09-02 20:13:50 +0200

    Add IOS support

--- webserver/assets/main.js
+++ webserver/assets/main.js
@@ -1,45 +1,32 @@
 const base_url = "/api/playlist.m3u8";
 
-
 document.addEventListener('DOMContentLoaded', function () {
-    // Check if HLS is supported in this browser
+  // Check if HLS is supported in this browser
+  const video = document.getElementById('player');
+    
+  document.getElementById('HLS-submit').addEventListener('click', function (event) {
+    event.preventDefault();
+    const form = document.getElementById('hls-form');
+    let url = base_url + "?"
+    for (let i = 0; i < form.elements.length; i++) {
+      const element = form.elements[i];
+      if (element.name && element.value) {
+        url += `&${element.name}=${encodeURIComponent(element.value)}`;
+      }
+    }
+
     if (Hls.isSupported()) {
         const hls = new Hls();
-        const video = document.getElementById('player');
-
-        document.getElementById('HLS-submit').addEventListener('click', function (event) {
-            event.preventDefault();
-
-            const form = document.getElementById('hls-form');
-
-            // Start building the URL for the request
-            let url = base_url + "?"
-
-            // Dynamically append other parameters
-            for (let i = 0; i < form.elements.length; i++) {
-                const element = form.elements[i];
-
-                if (element.name && element.value) {
-                    url += `&${element.name}=${encodeURIComponent(element.value)}`;
-                }
-            }
-
-            // Load the stream using hls.js
-            if (hls) {
-                hls.loadSource(url);
-                hls.attachMedia(video);
-                hls.on(Hls.Events.MANIFEST_PARSED, function () {
-                    video.play();
-                });
-            } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-                // If HLS isn't supported natively but the video tag can play it
-                video.src = url;
-                video.addEventListener('loadedmetadata', function () {
-                    video.play();
-                });
-            }
+        hls.loadSource(url);
+        hls.attachMedia(video);
+        hls.on(Hls.Events.MANIFEST_PARSED, function () {
+            video.play();
         });
-    } else {
-        alert('HLS is not supported in this browser!');
     }
+    else {
+        // Tries to load it using src, with luck the browser will be compatible
+        video.src = url;
+        video.play();
+    }
+  });
 });
