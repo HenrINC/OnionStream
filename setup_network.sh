d93f9df97510aaafb6ef8eee0787ad518a3cbc98
Henri Questiaux <henri.questiaux@gmail.com>
2024-01-08 22:22:50 +0100

    Fix ipvlan network using the computer's IP

--- setup_network.sh
+++ setup_network.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
+
 NETWORK_NAME="restreamer_network"
+RANGE_START="128"
+RANGE_LENGTH="26"
 
 # Check if the network exists
 if docker network inspect "$NETWORK_NAME" >/dev/null 2>&1; then
@@ -6,12 +10,19 @@ if docker network inspect "$NETWORK_NAME" >/dev/null 2>&1; then
     docker network rm "$NETWORK_NAME"
 fi
 
+# Get the default network interface, subnet, and gateway
 DEFAULT_INTERFACE=$(ip route | grep default | awk '{print $5}')
 SUBNET=$(ip -o -f inet addr show $DEFAULT_INTERFACE | awk '{print $4}')
 GATEWAY=$(ip route | grep default | awk '{print $3}')
 
+# Calculate IP range for Docker network
+IP_BASE=$(echo $SUBNET | cut -d'/' -f1 | awk -F. '{OFS="."; print $1,$2,$3}')
+IP_RANGE="${IP_BASE}.${RANGE_START}/${RANGE_LENGTH}"
+
+# Create Docker network with IPvLAN
 docker network create -d ipvlan \
     --subnet=$SUBNET \
+    --ip-range=$IP_RANGE \
     --gateway=$GATEWAY \
     -o parent=$DEFAULT_INTERFACE \
     $NETWORK_NAME
